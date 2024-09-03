0b73fe61a5577e024e710f6b653393172611e319
Ba6taTi <krasizh07@gmail.com>
2024-09-03 12:05:58 +0300

    Gradle Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0 Beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
